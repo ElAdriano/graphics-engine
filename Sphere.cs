65c4c26cb57826415e707c5bf7eaf662f94aded1
greenhoody <1mk2mk3mk@gmail.com>
2021-05-20 18:42:10 +0200

    troche wiecej

--- Sphere.cs
+++ Sphere.cs
@@ -7,6 +7,7 @@ using SharpDX;
 
 namespace VirtualCamera
 {
+    //sphere (x-a)^2 + (y-b)^2 + (z-c)^2 = r^2
     public class Sphere
     {
         public Vector3 Origin;
@@ -25,6 +26,14 @@ namespace VirtualCamera
             Vector3 normal = point - Origin;
             normal.Normalize();
             return normal;
+        }
+
+        public Vector3 getPoint(float x, float y)
+        {
+            //z = sqrt(r^2 - (x-a)^2 - (y-b)^2) + c
+            float z2c = (float)(Math.Pow(R, 2) - Math.Pow((x - Origin.X), 2) - Math.Pow((y - Origin.Y), 2));
+            float z = (float)Math.Sqrt(z2c) + Origin.Z;
+            return new Vector3(x, y, z);
         }
     }
 }
